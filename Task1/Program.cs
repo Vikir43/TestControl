75e3f20c4bc91b4ac95d47e753e2c47926e43791
Viki <viki333@list.ru>
2023-02-13 20:09:00 +0300

    Progect created  Array

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите массив строк  через пробел:  ");
+string[]array1 = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+PrintArray(array1);
+WriteLine(); 
+
+string[] array2 = new string[array1.Length];
+
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+if(array1[i].Length <=3 )
+    {
+      array2[count] = array1[i];
+      count++;
+    }
+}
+PrintArray(array2);
+
+void PrintArray(string[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]} ");
+    }
+    WriteLine("");
+}
+
